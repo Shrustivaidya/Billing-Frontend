f58c75bb94248b11524a76a2abd751f7a06176e9
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-07-01 10:02:35 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Homepage from './components/homepage/homepage';
 import Login from './components/login/login';
 import Register from './components/register/register';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     {/* <Homepage/> */}
-     <Login/>
-     {/* <Register/> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register/>} />
+          <Route path="/register" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
